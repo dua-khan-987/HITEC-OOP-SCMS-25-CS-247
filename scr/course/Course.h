082665b521fd8899ef28bcdfe7bb9b0730b30901
dua-khan-987 <25-cs-247@student.hitecuni.edu.pk>
2026-05-31 17:07:17 +0500

    [Course] Add Course class header

---
+++ scr/course/Course.h
@@ -0,0 +1,13 @@
+#ifndef COURSE_H
+#define COURSE_H
+#include <string>
+using namespace std;
+class Course{
+private:
+string courseCode;
+string courseName;
+int creditHours;
+public:
+Course();
+};
+#endif

8ba56bc6e5c96090cdd953ecb5b7ecb8ed17f70b
dua-khan-987 <25-cs-247@student.hitecuni.edu.pk>
2026-05-31 20:17:31 +0500

    [Utile] Add Reports.cpp

---
+++ scr/utils/Reports.cpp
@@ -0,0 +1,13 @@
+#include "Reports.h"
+#include <iostream>
+using namespace std;
+void Reports::studentReport()
+{ 
+cout << "Student Report" << endl;
+}
+void Reports::courseReport(){
+cout << "Course Report" << endl;
+}
+void Reports::financeReport(){
+cout << "Finance Report" << endl;
+}

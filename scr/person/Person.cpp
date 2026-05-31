54067c534c3f7b9269113ac8afc089dba8a9f723
dua-khan-987 <25-cs-247@student.hitecuni.edu.pk>
2026-05-31 20:44:17 +0500

    Update Person.cpp implementation

--- scr/person/Person.cpp
+++ scr/person/Person.cpp
@@ -1,6 +1,31 @@
-#include "person.h"
-Person::Person(string n, int a)
-{
-name=n;
-age=a;
+#include "Person.h"
+#include <iostream> 
+using namespace std;
+Person::Person()
+{ id = 0; name = ""; email = ""; } 
+Person::Person(int i, string n, string e)
+{ id = i; name = n; email = e;
+} 
+void Person::setId(int i)
+{ id = i;
+} 
+void Person::setName(string n) 
+{ name = n;
+} 
+void Person::setEmail(string e) 
+{ email = e;
+}
+int Person::getId()
+{ return id;
+}
+string Person::getName()
+{ return name;
+}
+string Person::getEmail()
+{ return email;
+} 
+void Person::display()
+{ cout << "ID: " << id << endl;
+cout << "Name: " << name << endl; 
+cout << "Email: " << email<<endl;
 }
